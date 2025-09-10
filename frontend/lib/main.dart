40abc3b2abeb34be506ccf128c7a735de303c0c2
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-10 19:21:47 +0530

    Backend Express Removed

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => BoxProvider())
       ],
       child: MaterialApp(
-        
         title: 'Zooma',
         theme: AppTheme.lightTheme,
         initialRoute: AppRoutes.home,
