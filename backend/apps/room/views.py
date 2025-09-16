a5dcdd2e01b0f19f766097f33992dbc13eec51e6
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-16 20:46:55 +0530

    Room View Updated

--- backend/apps/room/views.py
+++ backend/apps/room/views.py
@@ -2,16 +2,16 @@ from rest_framework import generics
 from .models import Room
 from .serializers import RoomSerializer
 from ..player.models import Player
+from rest_framework.request import Request
+from typing import cast
 
 class RoomCreateAPIView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
     def perform_create(self, serializer):
-        # CORRECT: Use request.data to get data from the POST body
-        mentor_id = self.request.data.get("mentor_id")
-
-
+        request = cast(Request, self.request)
+        mentor_id = request.data.get("mentor_id")
         mentor = Player.objects.get(id=mentor_id)
         room = serializer.save(mentor=mentor)
         room.players.add(mentor)
