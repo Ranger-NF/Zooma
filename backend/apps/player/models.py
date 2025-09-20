577970a2b8664c9df580f71636e7688ba64a190b
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-20 11:16:15 +0530

    player model Updated

--- backend/apps/player/models.py
+++ backend/apps/player/models.py
@@ -1,11 +1,9 @@
 import uuid
-
 from django.db import models
-# Create your models here.
 
 class Player(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
