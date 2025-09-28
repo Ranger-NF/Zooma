09d572532c0f97a4dd0bea7f1108376a682b099d
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-28 09:45:01 +0530

    Leader Board Service Created

--- frontend/lib/features/leaderboard/services/leaderboard_service.dart
+++ frontend/lib/features/leaderboard/services/leaderboard_service.dart
@@ -2,24 +2,24 @@ import '../../../core/network/api_service.dart';
 import '../models/leaderboard_model.dart';
 
 class LeaderboardService {
-  final DioClient _dioClient;
+  final ApiService apiService;
 
-  LeaderboardService(this._dioClient);
+  LeaderboardService(this.apiService);
 
   Future<List<LeaderboardEntry>> getRoomLeaderboard(String roomCode) async {
-    final response = await _dioClient.get('/api/rooms/$roomCode/leaderboard');
+    final response = await apiService.get('/api/rooms/$roomCode/leaderboard');
     return (response.data as List)
         .map((entry) => LeaderboardEntry.fromJson(entry))
         .toList();
   }
 
   Future<Map<String, dynamic>> getPlayerStats(String playerId) async {
-    final response = await _dioClient.get('/api/players/$playerId/stats');
+    final response = await apiService.get('/api/players/$playerId/stats');
     return response.data;
   }
 
   Future<Map<String, dynamic>> getRoomAnalytics(String roomCode) async {
-    final response = await _dioClient.get('/api/mentor/$roomCode/analytics');
+    final response = await apiService.get('/api/mentor/$roomCode/analytics');
     return response.data;
   }
 }
