3d72fb43078f63d3f20a5ab6cc383a2bad7a5a6f
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-28 22:26:44 +0530

    Register Page Design Almost Completed

--- frontend/lib/features/register/view/register.dart
+++ frontend/lib/features/register/view/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/theme/app_theme.dart';
 
 class RegisterScreen extends StatefulWidget{
   @override
@@ -6,8 +7,81 @@ class RegisterScreen extends StatefulWidget{
 }
 
 class _StateRegisterPage extends State<RegisterScreen>{
+
+  final TextEditingController _controller = TextEditingController();
+
+  @override 
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFFDD259),
+      body: LayoutBuilder(
+        builder: (BuildContext ctx, BoxConstraints view){
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: view.maxHeight
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Enter Your Name",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35
+                    ),
+                  ),
+                  Text(
+                    "To Start the Game..",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      ),
+                      elevation: 50,
+                      child: TextFormField(
+                        controller: _controller,
+                        style: TextStyle(
+                          color: AppTheme.backgroundColor,
+                          fontSize: 25
+                        ),
+                        cursorColor: AppTheme.backgroundColor,
+                        cursorWidth: 3,
+                        cursorHeight: 35,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(35),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(36),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    )
+                  )
+                ],
+              ),
+            ),
+          );
+        } 
+      ),
+    );
   }
 }
