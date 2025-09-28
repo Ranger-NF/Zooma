9d87c5e305f9435ca49e678c7396fbfe70770c77
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-28 21:32:12 +0530

    Task Service Updated

--- frontend/lib/features/tasks/services/task_service.dart
+++ frontend/lib/features/tasks/services/task_service.dart
@@ -2,33 +2,33 @@ import '../../../core/network/api_service.dart';
 import '../models/task_model.dart';
 
 class TaskService {
-  final DioClient _dioClient;
+  final ApiService apiService;
 
-  TaskService(this._dioClient);
+  TaskService(this.apiService);
 
   Future<List<Task>> addTasksToRoom(String roomCode, List<Map<String, dynamic>> tasks) async {
-    final response = await _dioClient.post('/api/rooms/$roomCode/tasks', data: {
+    final response = await apiService.post('/api/rooms/$roomCode/tasks', data: {
       'tasks': tasks,
     });
     return (response.data as List).map((task) => Task.fromJson(task)).toList();
   }
 
   Future<List<Task>> getPlayerTasks(String roomCode, String playerId) async {
-    final response = await _dioClient.get('/api/rooms/$roomCode/tasks/$playerId');
+    final response = await apiService.get('/api/rooms/$roomCode/tasks/$playerId');
     return (response.data as List).map((task) => Task.fromJson(task)).toList();
   }
 
   Future<List<Task>> getAllRoomTasks(String roomCode) async {
-    final response = await _dioClient.get('/api/mentor/$roomCode/tasks');
+    final response = await apiService.get('/api/mentor/$roomCode/tasks');
     return (response.data as List).map((task) => Task.fromJson(task)).toList();
   }
 
-  Future<Task> updateTask(String roomCode, String taskId, Map<String, dynamic> updates) async {
-    final response = await _dioClient.put('/api/rooms/$roomCode/tasks/$taskId', data: updates);
-    return Task.fromJson(response.data);
-  }
+  // Future<Task> updateTask(String roomCode, String taskId, Map<String, dynamic> updates) async {
+  //   final response = await apiService.put('/api/rooms/$roomCode/tasks/$taskId', data: updates);
+  //   return Task.fromJson(response.data);
+  // }
 
-  Future<void> deleteTask(String roomCode, String taskId) async {
-    await _dioClient.delete('/api/rooms/$roomCode/tasks/$taskId');
-  }
+  // Future<void> deleteTask(String roomCode, String taskId) async {
+  //   await apiService.delete('/api/rooms/$roomCode/tasks/$taskId');
+  // }
 }
