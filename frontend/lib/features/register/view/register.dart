09e63cda52c6b895e6e69179b3d7c8d6d5f9e7a0
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-28 21:36:00 +0530

    Register Page Started

---
+++ frontend/lib/features/register/view/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget{
+  @override
+  State<RegisterScreen> createState() => _StateRegisterPage();
+}
+
+class _StateRegisterPage extends State<RegisterScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
