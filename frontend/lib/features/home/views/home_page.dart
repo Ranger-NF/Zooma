de95e0f793aae584f645461f703043dd0d2f810d
Muflih-uk <muhammedmuflih875@gmail.com>
2025-09-30 15:31:42 +0530

    Home Page Completed

--- frontend/lib/features/home/views/home_page.dart
+++ frontend/lib/features/home/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/routes/app_routes.dart';
 import 'package:frontend/core/theme/app_theme.dart';
 import 'package:frontend/features/home/views/home_widget.dart';
 
@@ -6,75 +7,74 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 80,
-        leading: Padding(
-          padding: EdgeInsets.only(left: 30,bottom: 5),
-          child: IconButton(
-            style: IconButton.styleFrom(
-              elevation: 20,
-              foregroundColor: AppTheme.backgroundColor,
-              backgroundColor: AppTheme.primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30)
-              )
-            ),
-            onPressed: (){}, 
-            icon: Icon(Icons.arrow_back)
-          ),
-        )
-      ),
       backgroundColor: AppTheme.backgroundColor,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(left: 35, right: 35, bottom: 60),
-          child: Container(
-            width: 300,
-            decoration: BoxDecoration(
-              border: BoxBorder.all(
-                color: Colors.black,
-                width: 4
-              ),
-              color: AppTheme.primaryColor,
-            ),
-            child: Column(
-              spacing: 30,
-              children: [
-                SizedBox(height: 60,),
-                HomeWidget.textButton(name: "Create Room", onPress: (){}),
-                HomeWidget.textButton(name: "Join Room", onPress: (){}),
-                Padding(
-                  padding: EdgeInsets.only(right: 20),
-                  child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 8,
-                              offset: Offset(1, 3),
-                              spreadRadius: 1
-                            )
-                          ],
-                          color: AppTheme.backgroundColor,
-                          borderRadius: BorderRadius.circular(30)
-                        ),
-                      )
-                    ],
-                  ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 60, left: 35, right: 35, bottom: 60),
+            child: Container(
+              width: 300,
+              decoration: BoxDecoration(
+                border: BoxBorder.all(
+                  color: Colors.black,
+                  width: 4
                 ),
-                SizedBox(height: 60,),
-                HomeWidget.textButton(name: "Leaderboard", onPress: (){})
-
-              ],
+                color: AppTheme.primaryColor,
+              ),
+              child: Column(
+                spacing: 30,
+                children: [
+                  SizedBox(height: 60,),
+                  HomeWidget.textButton(
+                    name: "Create Room", 
+                    onPress: (){
+                      Navigator.pushNamed(context, AppRoutes.createRoom);
+                    }
+                  ),
+                  HomeWidget.textButton(
+                    name: "Join Room", 
+                    onPress: (){
+                      Navigator.pushNamed(context, AppRoutes.joinRoom);
+                    }
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 8,
+                                offset: Offset(1, 3),
+                                spreadRadius: 1
+                              )
+                            ],
+                            color: AppTheme.backgroundColor,
+                            borderRadius: BorderRadius.circular(30)
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 120,),
+                  HomeWidget.textButton(
+                    name: "Leaderboard", 
+                    onPress: (){
+                      Navigator.pushNamed(context, AppRoutes.leaderboard);
+                    }
+                  ),
+                  SizedBox(height: 60,)
+                ],
+              ),
             ),
-          ),
-        ), 
-      ),
+          ), 
+        ),
+      )
     );
   }
 }
