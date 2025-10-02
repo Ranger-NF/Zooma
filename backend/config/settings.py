378524bd4c2c07632a0138444ae5fc9aa948ca54
Muflih-uk <muhammedmuflih875@gmail.com>
2025-10-02 20:42:19 +0530

    Small Changes in Backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s&kov%s&rj($6v15*=f(hu4a14)$a5^g&dasyu5w5&msgud*i6"
+SECRET_KEY = "s&kov%s&rj($6v15*=f(hu4a14)$a5^g&dasyu5w5&msgud*i6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
